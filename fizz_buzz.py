69185d5200c56a65f9f1f070ce9fbef2171f7d39
Amen_bs <amenallah.bensalem@esprit.tn>
2022-11-05 15:13:35 +0100

    Update fizz_buzz.py

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,11 +1,11 @@
 #fizz_buzz
 
 for i in range(1,100):
-    if (i % 3 == 0) and (i % 5 == 0):
+    if (i % 3 == 0) and (i % 5 == 0): #check for multiple of 3 and  check for multiple of   5 
         print('FizzBuzz')
     elif i % 3 ==0 :
         print('Fizz')
     elif i % 5 == 0:
         print('Buzz')
     else:
-        print(i)
+        print(i)    #   print   the number  of times        
