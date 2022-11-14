cd05b92adec7b757f4097a1808de0ecfc2b0de7c
Amen_bs <115142481+TheBOYHowell@users.noreply.github.com>
2022-11-14 11:43:01 +0100

    Update scrape_python.py

--- scrape_python.py
+++ scrape_python.py
@@ -1,14 +1,17 @@
 # beauty_soup.py
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+m=0
 # https://www.tunisianet.com.tn/596-smartphone-mobile-4g-tunisie?page=j
 for j in range(1,8):
     url = f'https://www.tunisianet.com.tn/596-smartphone-mobile-4g-tunisie?page={j}'
     page = urlopen(url)
     html = page.read().decode("utf-8")
     soup = BeautifulSoup(html, "html.parser")
-    texts = soup.find_all("span", {"class": "product-reference"})
+    texts = soup.find_all("h2", {"class": "h3 product-title"})
     for text in texts:
          print(text.get_text())
+         m=m+1
+print("Nombre darticle",m)
 
 #scrape from site all reference information 
